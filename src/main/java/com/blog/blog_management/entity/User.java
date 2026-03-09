ace16bfbb62ee66ff98ac66779dbb3eea28cc5f6
Bharat <bharatprasad@Bharats-MacBook-Air.local>
2026-03-09 14:03:11 +0530

    Initial commit

---
+++ src/main/java/com/blog/blog_management/entity/User.java
@@ -0,0 +1,35 @@
+package com.blog.blog_management.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+    private Date createdAt;
+}
